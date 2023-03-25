8ba4982c74d366bdc89690ca868c2bcdbe5942f0
Icaruk <adri-rk@hotmail.com>
2023-03-25 19:54:54 +0100

    0.0.2

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,17 +3,18 @@ const fs = require("fs");
 const path = require("path");
 
 const platform = os.platform();
-const binPath = path.join(__dirname, "dist");
+const binPath = path.join(__dirname, "../dist");
+const basePath = path.join(__dirname, "..");
 
 switch (platform) {
 	case "win32":
-		fs.copyFileSync(path.join(binPath, "up-npm-v0.0.1-windows-amd64.exe"), path.join(__dirname, "up-npm.exe"));
+		fs.copyFileSync(path.join(binPath, "up-npm-0.0.1-windows-amd64.exe"), path.join(basePath, "up-npm.exe"));
 		break;
 	case "darwin":
-		fs.copyFileSync(path.join(binPath, "up-npm-v0.0.1-darwin-amd64"), path.join(__dirname, "up-npm"));
+		fs.copyFileSync(path.join(binPath, "up-npm-0.0.1-darwin-amd64"), path.join(basePath, "up-npm"));
 		break;
 	case "linux":
-		fs.copyFileSync(path.join(binPath, "up-npm-v0.0.1-linux-amd64"), path.join(__dirname, "up-npm"));
+		fs.copyFileSync(path.join(binPath, "up-npm-0.0.1-linux-amd64"), path.join(basePath, "up-npm"));
 		break;
 	default:
 		console.error("Unsupported platform:", platform);
