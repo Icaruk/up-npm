c104a46b91bcf3b298980d2d0d284dc986d22034
Icaruk <adri-rk@hotmail.com>
2023-03-26 18:49:32 +0200

    0.1.9

--- scripts/setup.js
+++ scripts/setup.js
@@ -6,8 +6,7 @@ const platform = os.platform();
 
 const distPath = path.join(__dirname, "../dist");
 const scriptsPath = path.join(__dirname, "../scripts");
-const basePath = path.join(__dirname, "..");
-const binPath = path.join(__dirname, "../bin");
+const binPath = path.join(__dirname);
 
 const appName = "up-npm";
 const version = "0.0.1";
@@ -32,17 +31,20 @@ switch (platform) {
 		process.exit(1);
 }
 
-
-if (!fs.existsSync(binPath)) {
-	fs.mkdirSync(binPath);
-}
-
 const binFilename = `${appName}${suffix}`;
 
-fs.copyFileSync(
-	path.join(distPath, distFilename),
-	path.join(basePath, "bin", binFilename),
-);
+const source = path.join(distPath, distFilename);
+const destination = path.join(binPath, binFilename);
+
+if (!fs.existsSync(source)) {
+	console.error(`${source} does not exist`);
+	process.exit(1);
+};
+
+
+if (fs.existsSync(destination)) fs.rmSync(destination)
+fs.copyFileSync(source, destination);
+
 
 // Cleanup
 // fs.rmdirSync(distPath, { recursive: true });
