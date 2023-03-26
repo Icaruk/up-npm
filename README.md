84ea3c2f62b585f4b0b88cd851dbfbc2cb63a20c
Icaruk <adri-rk@hotmail.com>
2023-03-26 18:18:12 +0200

    postinstall working

--- README.md
+++ README.md
@@ -1,17 +1,5 @@
 # up-npm
 
+Review and update your NPM dependencies.
+
 ![](https://i.imgur.com/MzzR05S.png)
-
-# todo
-
-- [x] No package.json found
-- [ ] No new versions found
-- [x] Unsupported/invalid version
-- [ ] Show changes
-  - [ ] Get repo url + /releases
-  - [x] Fallback to homepage
-  - [ ] No changes page found
-- [x] Get version prefix https://semver.npmjs.com/
-  - [x] Write again
-- [ ] Merge dependencies and devdeps to fetch them all
-- [ ] Parallel fetch
--- scripts/setup.js
+++ scripts/setup.js
@@ -3,20 +3,47 @@ const fs = require("fs");
 const path = require("path");
 
 const platform = os.platform();
-const binPath = path.join(__dirname, "../dist");
+
+const distPath = path.join(__dirname, "../dist");
+const scriptsPath = path.join(__dirname, "../scripts");
 const basePath = path.join(__dirname, "..");
+const binPath = path.join(__dirname, "../bin");
+
+const appName = "up-npm";
+const version = "0.0.1";
+
+let distFilename = "";
+let suffix = "";
+
 
 switch (platform) {
 	case "win32":
-		fs.copyFileSync(path.join(binPath, "up-npm-0.0.1-windows-amd64.exe"), path.join(basePath, "up-npm.exe"));
+		distFilename = `${appName}-${version}-windows-amd64.exe`;
+		suffix = ".exe";
 		break;
 	case "darwin":
-		fs.copyFileSync(path.join(binPath, "up-npm-0.0.1-darwin-amd64"), path.join(basePath, "up-npm"));
+		distFilename = `${appName}-${version}-darwin-amd64`;
 		break;
 	case "linux":
-		fs.copyFileSync(path.join(binPath, "up-npm-0.0.1-linux-amd64"), path.join(basePath, "up-npm"));
+		distFilename = `${appName}-${version}-linux-amd64`;
 		break;
 	default:
 		console.error("Unsupported platform:", platform);
 		process.exit(1);
 }
+
+
+if (!fs.existsSync(binPath)) {
+	fs.mkdirSync(binPath);
+}
+
+const binFilename = `${appName}${suffix}`;
+
+fs.copyFileSync(
+	path.join(distPath, distFilename),
+	path.join(basePath, "bin", binFilename),
+);
+
+// Cleanup
+fs.rmdirSync(distPath, { recursive: true });
+fs.rmdirSync(scriptsPath, { recursive: true });
