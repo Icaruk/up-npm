77d649b54df7fc904c21c1f99c88708ac3d42fae
Icaruk <adri-rk@hotmail.com>
2023-04-02 19:48:58 +0200

    2.3.0

--- scripts/setup.js
+++ scripts/setup.js
@@ -10,7 +10,7 @@ const scriptsPath = path.join(__dirname, "../scripts");
 const binPath = path.join(__dirname, "..");
 
 const appName = "up-npm";
-const version = "2.2.0";
+const version = "2.3.0";
 
 let distFilename = "";
 let isWindows = false;
